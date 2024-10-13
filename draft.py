cba981572018429b68d473c44b5179ab1322d89c
MaiklDudikov <wmiwa87@gmail.com>
2024-10-13 20:06:23 +0200

    Created models and performed migrations

--- draft.py
+++ draft.py
@@ -9,3 +9,12 @@ print('dz Django')
 # 4. python manage.py runserver
 
 # 5. python manage.py startapp hello
+# Зарегистрируйте приложение blog в файле blog_project/settings.py
+# Добавить файл urls.py приложение blog
+
+# 6. Создание моделей в models.py и потом миграции :
+# python manage.py makemigrations
+# python manage.py migrate
+
+# 7. python manage.py createsuperuser (mishawin11/12345678)
+# 8. зарегистрировать таблицу в admin.py
