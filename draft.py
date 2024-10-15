f795558788bbcebbc504f6f0b84f6d0fe271ed7a
MaiklDudikov <wmiwa87@gmail.com>
2024-10-15 13:21:21 +0200

    update dz_01_term_16.10.2024

--- draft.py
+++ draft.py
@@ -3,9 +3,9 @@ print('dz Django')
 # pip list
 
 # 1. pip install django
-# 2. django-admin startproject first_project
+# 2. django-admin startproject blog_project
 
-# 3. cd first_project
+# 3. cd blog_project
 # 4. python manage.py runserver
 
 # 5. python manage.py startapp hello
