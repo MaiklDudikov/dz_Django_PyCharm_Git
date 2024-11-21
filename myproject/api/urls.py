d4a9a41b276abaf7b88122619ea84ed87f18e2d4
MaiklDudikov <wmiwa87@gmail.com>
2024-11-21 19:43:16 +0200

    added predictions Starkov task_3_4

--- myproject/api/urls.py
+++ myproject/api/urls.py
@@ -7,4 +7,11 @@ urlpatterns = [
     path('random/', views.random_number, name='random_number'),
     path('random/range/', views.random_number_in_range, name='random_number_in_range'),
     path('random/set/', views.random_number_set, name='random_number_set'),
+    path('poem/random/', views.random_poem, name='random_poem'),
+    path('poem/author/', views.random_poem_by_author, name='random_poem_by_author'),
+    path('poem/theme/', views.random_poem_by_theme, name='random_poem_by_theme'),
+    path('poem/titles/author/', views.list_poems_by_author, name='list_poems_by_author'),
+    path('poem/authors/', views.list_authors, name='list_authors'),
+    path('poem/themes/', views.list_themes, name='list_themes'),
+    path('poem/titles/theme/', views.list_poems_by_theme, name='list_poems_by_theme'),
 ]
--- myproject/api/views.py
+++ myproject/api/views.py
@@ -34,3 +34,64 @@ def random_number_set(request):
     count = int(request.query_params.get('count', 5))
     numbers = [random.randint(1, 100) for _ in range(count)]
     return Response({"numbers": numbers})
+
+
+# Задание 3
+poems = {
+    "love": [
+        {"title": "Любовь", "author": "Пушкин", "text": "Я вас любил..."},
+        {"title": "Мгновенье", "author": "Тютчев", "text": "О как убийственно мы любим..."},
+    ],
+    "life": [
+        {"title": "Жизнь", "author": "Лермонтов", "text": "Как часто, пёстрою толпою окружён..."},
+        {"title": "Судьба", "author": "Блок", "text": "В те дни, когда мне были новы..."},
+    ],
+}
+
+
+@api_view(['GET'])
+def random_poem(request):
+    category = random.choice(list(poems.keys()))
+    poem = random.choice(poems[category])
+    return Response(poem)
+
+
+@api_view(['GET'])
+def random_poem_by_author(request):
+    author = request.query_params.get('author', 'Пушкин')
+    author_poems = [poem for poems_list in poems.values() for poem in poems_list if poem["author"] == author]
+    return Response(random.choice(author_poems) if author_poems else {"error": "Автор не найден"})
+
+
+@api_view(['GET'])
+def random_poem_by_theme(request):
+    theme = request.query_params.get('theme', 'love')
+    theme_poems = poems.get(theme, [])
+    return Response(random.choice(theme_poems) if theme_poems else {"error": "Тематика не найдена"})
+
+
+# Задание 4
+@api_view(['GET'])
+def list_poems_by_author(request):
+    author = request.query_params.get('author', 'Пушкин')
+    author_poems = [poem["title"] for poems_list in poems.values() for poem in poems_list if poem["author"] == author]
+    return Response({"titles": author_poems})
+
+
+@api_view(['GET'])
+def list_authors(request):
+    authors = list({poem["author"] for poems_list in poems.values() for poem in poems_list})
+    return Response({"authors": authors})
+
+
+@api_view(['GET'])
+def list_themes(request):
+    themes = list(poems.keys())
+    return Response({"themes": themes})
+
+
+@api_view(['GET'])
+def list_poems_by_theme(request):
+    theme = request.query_params.get('theme', 'love')
+    theme_poems = [poem["title"] for poem in poems.get(theme, [])]
+    return Response({"titles": theme_poems})
