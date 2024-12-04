48ef7c227302a30ad1c8b60b740a48181390a24c
MaiklDudikov <wmiwa87@gmail.com>
2024-12-04 19:50:34 +0200

    updated library_project

--- library_project/library/admin.py
+++ library_project/library/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Book, Reader
 
+# Register your models here.
 admin.site.register(Book)
 admin.site.register(Reader)
--- library_project/library/urls.py
+++ library_project/library/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/', views.all_books, name='all_books'),
+    path('', views.all_books, name='all_books'),
     path('books/available/', views.available_books, name='available_books'),
     path('readers/', views.all_readers, name='all_readers'),
 ]
